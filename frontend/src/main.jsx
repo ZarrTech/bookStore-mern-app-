06678aa653069557449a350fa2b951d8f7744616
Prosper Lazarus <prosperzarus@gmail.com>
2024-05-08 14:07:09 +0100

    notis

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import SnackbarProvider from "react-simple-snackbar";
+import SnackbarProvider from "notistack";
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
