89c6dadd416cc3056f5e296c3f79f44cea6c7994
Prosper Lazarus <prosperzarus@gmail.com>
2024-05-09 14:08:40 +0100

    .

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,12 @@ const errorHandlerMiddleware = require("./middleware/errorHandlingMiddleware");
 const app = express();
 
 // middleware
-app.use(cors())
+const corsOptions = {
+  origin: "https://book-store-mern-frontend-pearl.vercel.app",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
