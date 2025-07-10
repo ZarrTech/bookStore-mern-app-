3312a4278d717e5df0786dc832bc1d3879b6b4f5
Zarrtech <prosperzarus@gmail.com>
2025-07-10 19:48:00 +0100

    another fix on api url

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -5,7 +5,7 @@ import BookTable from "../components/home/BookTable";
 import useFetch from '../useFetch/useFetch'
 import { useState } from "react";
 import BookCard from "../components/home/BookCard";
-export const url = import.meta.env.VITE_API_URL
+export const url = "http://api.lazaai.xyz:3000/books"
 const Home = () => {
   const [ShowType, setShowType] = useState('table')
   const { books, isloading, error } = useFetch(url);
